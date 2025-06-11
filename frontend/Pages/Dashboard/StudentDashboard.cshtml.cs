2cf54b667e2f04cb689b516fe8dc54ce9f36fb9c
TOWILL01210 <komiwilfriedagbota@gmail.com>
2025-06-11 16:49:57 +0000

    le dashboard étudiant

---
+++ frontend/Pages/Dashboard/StudentDashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace frontend.Pages.Dashboard
+{
+    public class StudentDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Code à exécuter lors du chargement GET de la page
+        }
+    }
+}
